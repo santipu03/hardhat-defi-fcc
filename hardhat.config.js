b6415da6e909faab71f63a070cd800012e1e665f
santipu <santipujolsau@gmail.com>
2022-10-24 19:40:47 +0200

    added forking config of eth mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
@@ -21,6 +22,12 @@ module.exports = {
         hardhat: {
             chainId: 31337,
             blockConfirmations: 1,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        localhost: {
+            chainId: 31337,
         },
         goerli: {
             url: GOERLI_RPC_URL,
